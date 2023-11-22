cde09ec2cdd4c7cb4b6515d22c341af09910c1c3
Ashey <114530755+HughsA@users.noreply.github.com>
2023-11-22 17:05:30 -0600

    microservice addition

--- main.js
+++ main.js
@@ -23,3 +23,38 @@ button.addEventListener('click', () => {
       hoverMessage.style.display = 'block';
     }
   });
+
+  // Function to open a popup with poem data
+function openPopupWithData(poemTitle, poemData) {
+  const popupWindow = window.open('', 'Poem Popup', 'width=600, height=400');
+  popupWindow.document.write('<html><head><title>' + poemTitle + '</title></head><body>');
+  popupWindow.document.write('<div class="popup"><h1>' + poemTitle + '</h1>');
+  popupWindow.document.write('<p>' + poemData + '</p></div>'); // Display the poem data
+  popupWindow.document.write('</body></html>');
+}
+
+// Function to fetch poem data and open popup
+function fetchAndDisplayPoem(poemTitle) {
+  fetch(`http://127.0.0.1:5000/poe_search?title=${encodeURIComponent(poemTitle)}`)
+      .then(response => response.json())
+      .then(data => {
+          // Assuming 'data' contains the poem text or related information
+          openPopupWithData(poemTitle, data);
+      })
+      .catch(error => console.error('Error:', error));
+}
+
+// Add event listener for submitting a poem title
+const submitButton = document.getElementById('submitButton');
+submitButton.addEventListener('click', function() {
+  const poemTitle = document.getElementById('messageInput').value;
+  fetchAndDisplayPoem(poemTitle);
+});
+
+// Add event listeners for selecting a poem title from the list
+document.querySelectorAll('.poemTitle').forEach(item => {
+  item.addEventListener('click', function() {
+      const poemTitle = this.textContent;
+      fetchAndDisplayPoem(poemTitle);
+  });
+});
